639da716b0ee2db033a9a3aeff1bc418e7459714
galiia-k <91274902+galiia-k@users.noreply.github.com>
2021-10-31 22:38:55 +0300

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-document = pd.read_csv('C://Users//Мвидео//Downloads//transactions.csv')
+document = pd.read_csv('transactions.csv')
 filt = document['STATUS'] == 'OK'
 first = document.loc[filt].sort_values(by='SUM', ascending=False).head(3)
 filt2 = document['CONTRACTOR'] == 'Umbrella, Inc'
